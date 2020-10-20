bafbafd70d1c9cb19120e2db4378a7e285550993
vrto <michal.vrtiak+github@gmail.com>
2020-10-20 16:57:12 +0200

    Introduce OBJECTS_DIR constant

--- src/main/kotlin/kgit/Data.kt
+++ src/main/kotlin/kgit/Data.kt
@@ -5,6 +5,7 @@ import java.nio.file.Path
 import java.security.MessageDigest
 
 const val KGIT_DIR = ".kgit"
+const val OBJECTS_DIR = ".kgit/objects"
 
 object Data {
 
@@ -21,21 +22,21 @@ object Data {
         val oid = digest.joinToString(separator = "") { "%02x".format(it) }
 
         // write actual binary content using the OID
-        val obj = Files.createFile(Path.of("$KGIT_DIR/objects/$oid"))
+        val obj = Files.createFile(Path.of("$OBJECTS_DIR/$oid"))
         Files.write(obj, data)
 
         return oid
     }
 
     private fun ensureObjectsDirectory() {
-        val objectsDirectory = Path.of("$KGIT_DIR/objects")
+        val objectsDirectory = Path.of(OBJECTS_DIR)
         if (!Files.exists(objectsDirectory)) {
             Files.createDirectory(objectsDirectory)
         }
     }
 
     fun getObject(oid: String): String {
-        val obj = Path.of("$KGIT_DIR/objects/$oid")
+        val obj = Path.of("$OBJECTS_DIR/$oid")
         return String(Files.readAllBytes(obj))
     }
 }
