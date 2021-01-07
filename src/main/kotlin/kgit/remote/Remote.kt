32184c9dbb86b528fb71bc83156f856516d54451
vrto <michal.vrtiak+github@gmail.com>
2021-01-07 09:29:54 +0100

    Refactor Remote

--- src/main/kotlin/kgit/remote/Remote.kt
+++ src/main/kotlin/kgit/remote/Remote.kt
@@ -4,10 +4,12 @@ import kgit.data.ObjectDatabase
 
 class Remote {
 
-    fun fetch(remotePath: String): List<String> {
+    fun fetch(remotePath: String): List<String> = getRemoteRefs(remotePath, prefix = "heads/")
+
+    private fun getRemoteRefs(remotePath: String, prefix: String): List<String> {
         val remoteData = ObjectDatabase(remotePath)
         return remoteData.iterateRefs()
             .map { it.name }
-            .filter { it.contains("heads/") }
+            .filter { it.contains(prefix) }
     }
 }
